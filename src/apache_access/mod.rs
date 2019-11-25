0c187455ef670d436fe4bcd44cb97355b1ed43e1
Connor Peticca <cjpet10@gmail.com>
2019-11-25 13:13:45 -0500

    adds empty quote test

--- src/apache_access/mod.rs
+++ src/apache_access/mod.rs
@@ -37,7 +37,7 @@ mod tests {
     use std::io::Read;
 
     #[test]
-    fn full_line() {
+    fn single() {
         let line = r#"83.149.9.216 - - [17/May/2015:10:05:03 +0000] "GET /presentations/logstash-monitorama-2013/images/kibana-search.png HTTP/1.1" 200 203023 "http://semicomplete.com/presentations/logstash-monitorama-2013/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/32.0.1700.77 Safari/537.36""#;
         let target = Log {
             ip: "83.149.9.216",
@@ -55,7 +55,7 @@ mod tests {
     }
 
     #[test]
-    fn all_sample_lines() {
+    fn all_samples() {
         let mut buf = String::new();
         File::open("samples/apache_access.txt")
             .unwrap()
@@ -68,6 +68,24 @@ mod tests {
                 panic!("failed to parse: {}", line);
             }
         }
+    }
+
+    #[test]
+    fn empty_quotes() {
+        let line = r#"83.149.9.216 - - [] "" 200 203023 "" """#;
+        let target = Log {
+            ip: "83.149.9.216",
+            identity: "-",
+            user: "-",
+            date: "",
+            message: "",
+            status: "200",
+            code: "203023",
+            referer: "",
+            user_agent: ""
+        };
+
+        assert_eq!(Stage2::new(line.as_bytes()).parse(), Some(target));
     }
 
 }
